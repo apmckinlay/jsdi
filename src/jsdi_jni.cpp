011cc020c384548fd1cb7c6acf336a9fd35f2457
vcschapp <vcschapp@localhost>
2013-07-11 00:37:41 +0100

    Remove JNI hooks for suneido.language.jsdi.type.TypeFactory (the Java class no longer declares any native methods). General cleanup.

--- src/jsdi_jni.cpp
+++ src/jsdi_jni.cpp
@@ -43,72 +43,10 @@ JNIEXPORT jstring JNICALL Java_suneido_language_jsdi_JSDI_when
 }
 
 //==============================================================================
-//            JAVA CLASS: suneido.language.jsdi.type.TypeFactory
-//==============================================================================
-
-#include "gen/suneido_language_jsdi_type_TypeFactory.h"
-    // This #include isn't strictly necessary -- the only caller of these
-    // functions is the JVM. However, it is useful to have the generated code
-    // around.
-
-JNIEXPORT void JNICALL Java_suneido_language_jsdi_type_TypeFactory_releaseHandle
-  (JNIEnv *, jclass, jlong jsdiHandle)
-{
-    type_descriptor * ptr = reinterpret_cast<type_descriptor *>(jsdiHandle);
-    assert(ptr->is_valid());
-        // TODO: assert that ptr is not a non-deletable type e.g. basic_value*
-    delete ptr;
-}
-
-JNIEXPORT jlong JNICALL Java_suneido_language_jsdi_type_TypeFactory_getBasicValueHandle
-  (JNIEnv * env, jclass, jobject basic_type)
-{
-    const suneido_language_jsdi_type_BasicType basic_type_(
-        jni_enum_to_cpp<suneido_language_jsdi_type_BasicType>(
-            env,
-            global_refs::ptr->suneido_language_jsdi_type_BasicType(),
-            basic_type)
-    );
-    const basic_value& result(basic_value::instance(basic_type_));
-    // The value returned is a pointer to a persistent type. It is never freed.
-    return reinterpret_cast<jlong>(&result);
-}
-
-JNIEXPORT jlong JNICALL Java_suneido_language_jsdi_type_TypeFactory_getBasicPointerHandle
-  (JNIEnv * env, jclass, jobject basic_type)
-{
-    const suneido_language_jsdi_type_BasicType basic_type_(
-        jni_enum_to_cpp<suneido_language_jsdi_type_BasicType>(
-            env,
-            global_refs::ptr->suneido_language_jsdi_type_BasicType(),
-            basic_type)
-    );
-    const basic_pointer * result(basic_value::instance(basic_type_).pointer_type());
-    // The value returned is a pointer to a persistent type. It is never freed.
-    return reinterpret_cast<jlong>(result);
-
-}
-
-JNIEXPORT jlong JNICALL Java_suneido_language_jsdi_type_TypeFactory_makeBasicArrayHandle
-  (JNIEnv * env, jclass, jobject basic_type, jint num_elems)
-{
-    const suneido_language_jsdi_type_BasicType basic_type_(
-        jni_enum_to_cpp<suneido_language_jsdi_type_BasicType>(
-            env,
-            global_refs::ptr->suneido_language_jsdi_type_BasicType(),
-            basic_type)
-    );
-    const basic_value& underlying(basic_value::instance(basic_type_));
-    // This pointer is now owned by the Java-side. It is the responsibility of
-    // the owning Java object to free it when the Java object becomes garbage.
-    return reinterpret_cast<jlong>(new basic_array(&underlying, num_elems));
-}
-
-//==============================================================================
 //                JAVA CLASS: suneido.language.jsdi.type.Type
 //==============================================================================
 
-#include "gen/suneido_language_jsdi_type_TypeFactory.h"
+#include "gen/suneido_language_jsdi_type_Type.h"
     // This #include isn't strictly necessary -- the only caller of these
     // functions is the JVM. However, it is useful to have the generated code
     // around.
