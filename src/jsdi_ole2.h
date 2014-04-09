f7ab25cb3aa57e0562f566387b8602e039fb9d8d
vcschapp <vcschapp@localhost>
2014-04-08 21:09:39 -0700

    MSVC: Refactor to compile under Microsoft Visual C++ compiler v.18.00.21114 for x86 (November 2013 CTP)
    - Because <ole2.h> has some of the same issues as <windows.h> (#define's the macros 'min' and 'max', for example), wrap it in a file, "jsdi_ole2.h" in same way <windows.h> is wrapped in "jsdi_windows.h".
    - As part of this process, factor out the common lines from "jsdi_windows.h" into "jsdi_windef.h".

---
+++ src/jsdi_ole2.h
@@ -0,0 +1,22 @@
+/* Copyright 2014 (c) Suneido Software Corp. All rights reserved.
+ * Licensed under GPLv2.
+ */
+
+//==============================================================================
+// file: jsdi_ole2.h
+// auth: Victor Schappert
+// date: 20140406
+// desc: Don't #include <ole2.h> directly. Instead include this file.
+//==============================================================================
+
+#ifndef __INCLUDED_JSDI_OLE2_H__
+#define __INCLUDED_JSDI_OLE2_H__
+
+#include "jsdi_windef.h"
+
+#include <ole2.h>
+
+#undef min
+#undef max
+
+#endif // __INCLUDED_JSDI_OLE2_H__
