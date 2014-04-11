eb6f6a06fabb0bd722990a0a1880653255cc634d
vcschapp <vcschapp@localhost>
2014-04-10 23:03:37 -0700

    MSVC: Refactor to compile under Microsoft Visual C++ compiler v.18.00.21114 for x86 (November 2013 CTP)
    - Implement MSVC versions of __stdcall function invocation assembly in 'stdcall_invoke.h'.
    - Minor related cleanup in 'heap.cpp'.

--- src/stdcall_invoke.h
+++ src/stdcall_invoke.h
@@ -84,7 +84,11 @@ struct stdcall_invoke
 inline uint64_t stdcall_invoke::basic(int args_size_bytes, char * args_ptr,
                                       void * func_ptr)
 {
-    uint64_t result;
+    union
+    {
+        uint32_t result32[2];
+        uint64_t result64;
+    };
     assert(
         0 == args_size_bytes % 4
             || !"argument size must be a multiple of 4 bytes");
@@ -97,24 +101,47 @@ inline uint64_t stdcall_invoke::basic(int args_size_bytes, char * args_ptr,
         /*                   the next word to push */
         /*                %2 is the base address of the array */
         /*                %3 is the address of the function to call */
-            "testl %1, %1    # If zero argument bytes given, skip \n\t"
-            "je    2f        # right to the function call.        \n\t"
+            "testl %1, %1        # If zero argument bytes given, skip  \n\t"
+            "je    2f            # right to the function call.         \n\t"
             "addl  %2, %1\n"
         "1:\n\t"
-            "subl  $4, %1    # Push arguments onto the stack in   \n\t"
-            "pushl (%1)      # reverse order. Keep looping while  \n\t"
-            "cmp   %2, %1    # addr to push (%1) > base addr (%2) \n\t"
-            "jg    1b        # Callee cleans up b/c __stdcall.    \n"
+            "subl  $4, %1        # Push arguments onto the stack in    \n\t"
+            "pushl (%1)          # reverse order. Keep looping while   \n\t"
+            "cmp   %2, %1        # addr to push (%1) > base addr (%2). \n\t"
+            "jg    1b            # Callee cleans up b/c __stdcall.     \n"
         "2:\n\t"
             "call  * %3"
-        : "=A" (result)
+        : "=A" (result64)
         : "r" (args_size_bytes), "r" (args_ptr), "r" (func_ptr)
         : "%ecx" /* eax, ecx, edx are caller-save */, "cc", "memory"
     );
+#elif defined(_MSC_VER)
+    // The assembly code below tries to clobber only the registers that are
+    // caller save under __stdcall anyway: eax, ecx, and edx. Hopefully this
+    // will cause MSVC to generate the minimum amount of register preserving
+    // code around this block.
+    __asm
+    {
+        mov   eax, args_size_bytes
+        test  eax, eax           // If zero argument bytes given, skip
+        je    ms_asm_basic_call  // right to the funtion call.
+        mov   edx, args_ptr
+        add   eax, edx
+    ms_asm_basic_loop:
+        sub   eax, 4             // Push arguments onto the stack in
+        push  dword ptr [eax]    // reverse order. Keep looping while
+        cmp   eax, edx           // addr to push (eax) > base addr (edx).
+        jg    ms_asm_basic_loop  // Callee cleans up b/c __stdcall.
+    ms_asm_basic_call:
+        mov   ecx, func_ptr      // Might as well clobber ecx since it is caller
+        call  ecx                // save under __stdcall anyway.
+        mov   eax, result32[0]
+        mov   edx, result32[1]
+    }
 #else
 #error Replacement for inline assembler required
 #endif
-    return result;
+    return result64;
 }
 
 inline double stdcall_invoke::return_double(int args_size_bytes,
@@ -133,21 +160,39 @@ inline double stdcall_invoke::return_double(int args_size_bytes,
         /*                   the next word to push */
         /*                %2 is the base address of the array */
         /*                %3 is the address of the function to call */
-            "testl %1, %1    # If zero argument bytes given, skip \n\t"
-            "je    2f        # right to the function call.        \n\t"
+            "testl %1, %1        # If zero argument bytes given, skip \n\t"
+            "je    2f            # right to the function call.        \n\t"
             "addl  %2, %1\n"
         "1:\n\t"
-            "subl  $4, %1    # Push arguments onto the stack in   \n\t"
-            "pushl (%1)      # reverse order. Keep looping while  \n\t"
-            "cmp   %2, %1    # addr to push (%1) > base addr (%2) \n\t"
-            "jg    1b        # Callee cleans up b/c __stdcall.    \n"
+            "subl  $4, %1        # Push arguments onto the stack in   \n\t"
+            "pushl (%1)          # reverse order. Keep looping while  \n\t"
+            "cmp   %2, %1        # addr to push (%1) > base addr (%2) \n\t"
+            "jg    1b            # Callee cleans up b/c __stdcall.    \n"
         "2:\n\t"
-            "call  * %3      # Callee will leave result in ST0"
+            "call  * %3          # Callee will leave result in ST0."
         : "=t" (result)
         : "r" (args_size_bytes), "r" (args_ptr), "r" (func_ptr)
         : "%eax", "%edx", "%ecx" /* eax, ecx, edx are caller-save */, "cc",
           "memory"
     );
+#elif defined(_MSC_VER)
+    __asm
+    {
+        mov   eax, args_size_bytes
+        test  eax, eax           // If zero argument bytes given, skip
+        je    ms_asm_double_call // right to the funtion call.
+        mov   edx, args_ptr
+        add   eax, edx
+    ms_asm_double_loop:
+        sub   eax, 4             // Push arguments onto the stack in
+        push  dword ptr [eax]    // reverse order. Keep looping while
+        cmp   eax, edx           // addr to push (eax) > base addr (edx).
+        jg    ms_asm_double_loop // Callee cleans up b/c __stdcall.
+    ms_asm_double_call:
+        mov   ecx, func_ptr      // Might as well clobber ecx since it is caller
+        call  ecx                // save under __stdcall anyway.
+        fst   result             // Callee will leave result in ST0.
+    }
 #else
 #error Replacement for inline assembler required
 #endif
