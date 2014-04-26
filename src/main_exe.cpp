6a41dae7035f049d5985ab7859ec309155a0712f
vcschapp <vcschapp@localhost>
2014-04-26 13:34:10 -0700

    Clean up and simplify .exe entry-point so it handles exceptions better and is less verbose.

--- src/main_exe.cpp
+++ src/main_exe.cpp
@@ -14,34 +14,59 @@
 #include <iostream>
 #include <cstring>
 
+namespace {
+
+void exception(const char * suite_name, const char * test_name,
+               const char * what)
+{
+    std::cerr << "main() caught exception running ";
+    if (suite_name)
+    {
+        std::cerr << suite_name;
+        if (test_name) std::cerr << '@' << test_name;
+    }
+    else std::cerr << "all tests";
+    std::cerr << ": " << what << std::endl;
+}
+
+void run(const char * suite_name, const char * test_name, int& exit_code)
+{
+    try
+    {
+        if (nullptr == suite_name)
+            jsdi::test_manager::instance().run_all();
+        else if (nullptr == test_name)
+            jsdi::test_manager::instance().run_suite(suite_name);
+        else
+            jsdi::test_manager::instance().run_test(suite_name, test_name);
+        return;
+    }
+    catch (const std::exception& e)
+    { exception(suite_name, test_name, e.what()); }
+    catch (const char * e)
+    { exception(suite_name, test_name, e); }
+    catch (...)
+    { exception(suite_name, test_name, "..."); }
+    exit_code = -1; // Return -1 if any exception is thrown.
+}
+
+} // anonymous namespace
+
 int main(int argc, char * argv[])
 {
     int return_value(0);
     if (argc < 2)
-        jsdi::test_manager::instance().run_all();
+        run(nullptr, nullptr, return_value);
     else for (int k = 1; k < argc; ++k)
     {
         char * at_sign = std::strchr(argv[k], '@');
         if (at_sign)
         {
             *at_sign = '\0';
-            try
-            { jsdi::test_manager::instance().run_test(argv[k], at_sign + 1); }
-            catch (const std::exception& e)
-            {
-                std::cerr << e.what() << std::endl;
-                return_value = -1;
-            }
+            run(argv[k], at_sign + 1, return_value);
         }
-        else try
-        { jsdi::test_manager::instance().run_suite(argv[k]); }
-        catch (const std::exception& e)
-        {
-            std::cerr << e.what() << std::endl;
-            return_value = -1;
-        }
+        else run(argv[k], nullptr, return_value);
     }
     jsdi::test_manager::instance().dump_report(std::cout);
     return jsdi::test_manager::instance().num_tests_failed() ? 1 : return_value;
 }
-
