9c705cb8df6157f51e08878908d1efec05f62b73
vcschapp <vcschapp@localhost>
2014-07-24 09:48:23 -0700

    FIX BUG: version::PLATFORM was still horribly wrong

--- src/version.cpp
+++ src/version.cpp
@@ -15,8 +15,10 @@ char const * const version::BUILD_DATE = __DATE__ " " __TIME__;
 char const * const version::PLATFORM =
 #if defined(_M_AMD64)
 "amd64"
-#else if defined(_M_IX86)
+#elif defined(_M_IX86)
 "x86"
+#else
+#error "unknown platform"
 #endif
 ;
 
